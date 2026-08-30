baac980ef90b3ecb1390f4b689d5642672956c2f
Yogendra Sai <yogendrasai02@gmail.com>
2026-08-30 19:04:33 +0530

    wiring up postgres db for local dev through docker

--- backend/src/main/java/com/financetracker/BackendApplication.java
+++ backend/src/main/java/com/financetracker/BackendApplication.java
@@ -3,12 +3,8 @@ package com.financetracker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.flyway.autoconfigure.FlywayAutoConfiguration;
-import org.springframework.boot.hibernate.autoconfigure.HibernateJpaAutoConfiguration;
-import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
 @SpringBootApplication(exclude = {
-		DataSourceAutoConfiguration.class,
-		HibernateJpaAutoConfiguration.class,
 		FlywayAutoConfiguration.class
 })
 public class BackendApplication {
