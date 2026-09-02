11600a2bff7752c97236855fb4886d6af345bcda
Yogendra Sai <yogendrasai02@gmail.com>
2026-09-02 23:23:55 +0530

    pointing application and flyway to the right db accounts

--- backend/src/main/java/com/financetracker/BackendApplication.java
+++ backend/src/main/java/com/financetracker/BackendApplication.java
@@ -2,11 +2,8 @@ package com.financetracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.flyway.autoconfigure.FlywayAutoConfiguration;
 
-@SpringBootApplication(exclude = {
-		FlywayAutoConfiguration.class
-})
+@SpringBootApplication
 public class BackendApplication {
 
 	public static void main(String[] args) {
